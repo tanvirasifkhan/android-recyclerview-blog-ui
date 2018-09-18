1a2809ecb4b843d321ec9f7fb6efc5f9cc37ebac
Asif Khan <asifkhan.github@gmail.com>
2018-09-18 22:22:56 +0600

    recyclerview click event added

--- app/src/main/java/com/example/asifkhan/androidrecyclerviewblog/adapters/BlogAdapter.java
+++ app/src/main/java/com/example/asifkhan/androidrecyclerviewblog/adapters/BlogAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 public class BlogAdapter extends RecyclerView.Adapter<BlogAdapter.DataHolder> {
     private ArrayList<Blog> blogs;
     private Context context;
+    private static int CROSS_FADE_DURATION=1000;
 
     public BlogAdapter(ArrayList<Blog> blogs, Context context) {
         this.blogs = blogs;
@@ -29,7 +31,7 @@ public class BlogAdapter extends RecyclerView.Adapter<BlogAdapter.DataHolder> {
     @Override
     public DataHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View  view=LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_blog_layout,parent,false);
-        return new DataHolder(view);
+        return new DataHolder(view,context,blogs);
     }
 
     @Override
@@ -40,7 +42,7 @@ public class BlogAdapter extends RecyclerView.Adapter<BlogAdapter.DataHolder> {
         holder.likes.setText(String.valueOf(blog.getLikes()));
         holder.comments.setText(String.valueOf(blog.getComments()));
         holder.shares.setText(String.valueOf(blog.getShares()));
-        Glide.with(context).load(blog.getPhoto()).crossFade(1000).diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.blog_photo);
+        Glide.with(context).load(blog.getPhoto()).crossFade(CROSS_FADE_DURATION).diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.blog_photo);
     }
 
     @Override
@@ -48,17 +50,28 @@ public class BlogAdapter extends RecyclerView.Adapter<BlogAdapter.DataHolder> {
         return blogs.size();
     }
 
-    class DataHolder extends RecyclerView.ViewHolder{
+    class DataHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         ImageView blog_photo;
         TextView title,author,likes,comments,shares;
-        public DataHolder(View itemView) {
+        Context context;
+        ArrayList<Blog> blogs;
+        public DataHolder(View itemView,Context context,ArrayList<Blog> blogs) {
             super(itemView);
+            this.context=context;
+            this.blogs=blogs;
+            itemView.setOnClickListener(this);
             blog_photo=(ImageView)itemView.findViewById(R.id.blog_photo);
             title=(TextView)itemView.findViewById(R.id.blog_title);
             author=(TextView)itemView.findViewById(R.id.blog_author);
             likes=(TextView)itemView.findViewById(R.id.likes);
             comments=(TextView)itemView.findViewById(R.id.comments);
             shares=(TextView)itemView.findViewById(R.id.shares);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Blog blog=this.blogs.get(getAdapterPosition());
+            Toast.makeText(context,blog.getTitle() , Toast.LENGTH_SHORT).show();
         }
     }
 }
